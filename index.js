aaf4a34b5dba3f58ce6a6b8e6528a7f8cf25310b
Ubuntu <ubuntu@ip-172-31-26-44.us-west-2.compute.internal>
2016-03-14 22:14:05 +0000

    test

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get('/', function(request, response) {
 response.sendfile('index.html') 
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080
+
+app.listen(port, function(){ 
+    console.log("Node app is running at localhost:" + port)
 })
