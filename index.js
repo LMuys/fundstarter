6d8973a370af5b801888dec55aacc30b89c52296
Ubuntu <ubuntu@ip-172-31-26-44.us-west-2.compute.internal>
2016-03-17 21:09:52 +0000

    Part 2 with read and writing to buffer.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 var fs = require('fs');
 var http = require('http');
-var index = fs.readFileSync('index.html', 'utf8');
+var buffer = new Buffer(1024);
+var indexString = '';
 
 var requestListener = function(req, res){
-    res.writeHead(200);
-    res.end(index);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.stat('index.html', function(err, stats) {
+	if (err) {
+	    return console.error(err);
+	    }
+	var buffer = new Buffer(stats.size);
+	var length = buffer.length;
+
+	fs.open('index.html', 'r', function(err, fd) {
+	    if (err) {
+		return console.error(err);
+	    }
+	    console.log("File opened.");
+	    fs.read(fd, buffer, 0, length, null, function(err, bytesRead, buff) {
+		if (err) {
+		    return console.error(err);
+		    }
+		console.log("File being read and written to HTTP response.");
+		res.write(buff.toString("utf8", 0, bytesRead));
+		res.end();
+		});
+	    });
+	});
 }    
 
 var server = http.createServer(requestListener);
-server.listen(8080);
+server.listen(process.env.PORT || 8080);
